1b81e34b99ae8638b877721fd69880e0b88ce95d
Patrick Brennan <pbrennanmusic@gmail.com>
2021-07-24 14:21:43 -0400

    commit tree 0a3682f1ca42b8dd81d5d82e374d1b37563a13d3

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,22 @@
 class StudentsController < ApplicationController
   before_action :set_student, only: :show
-  
+
   def index
     @students = Student.all
   end
 
   def show
+  end
+
+  def activate
+    @student = Student.find(params[:id])
+    if !@student.active
+      @student.update(active: true)
+      redirect_to student_path
+    else
+      @student.update(active: false)
+      redirect_to student_path
+    end
   end
 
   private
